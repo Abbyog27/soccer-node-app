010045f09d66a91486f6da8ae0bba1e489cac63e
abbyog27 <you@example.com>
2023-10-15 17:23:23 -0500

    feat: created text file, tested using core module and used core modules and req.query

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
-
+//test using the core module (fs)
+fs.readFileSync('soccer-players.txt', 'utf8', ( error, data) => {
+    if (error) {
+        console.log('------error --------', error);
+    } else {
+        console.log(data);
+    }
+})
 
 //creating home route
 app.get('/', (req, res) => {
     return res.json({message: 'Welcome to Soccer Node App'})
 });
 
+//using core modules and req.query
+app.get('/read', (req, res) => {
+    // grab the querry string, 
+    //pass the querystring into the fs function
+    //return data that comes from the txt file
+    let element = req.query.soccer;
+    fs.readFile(`${element}.txt`, 'utf8', (error, data) => {
+        if (error) {
+            return res.json({ message: "Error aquiring data, try again later"});
+        } else {
+            return res.json({ message: data});
+        }
+        });
+    });
 
 
 //set up a PORT number and listen for server 
